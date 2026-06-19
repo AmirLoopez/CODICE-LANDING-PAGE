12f22bac65ed3b24ced1a21d26a08ce602d450d6
AmirLoopez <144493609+AmirLoopez@users.noreply.github.com>
2026-06-19 11:52:45 -0600

    feat(web) adjust styling

--- obj/Debug/net10.0/CODICE.AssemblyInfo.cs
+++ obj/Debug/net10.0/CODICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CODICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b44dfdb9be05a88ad8f753048a13ec246c9d0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CODICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CODICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
